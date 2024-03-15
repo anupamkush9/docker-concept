2b920149ae4658a12d935c4bccae29b4cd76594e
Anupamkush9 <anupam.kush9@gmail.com>
2024-03-16 00:59:26 +0530

    updated docker-compose.yml file

--- docker_test_app/views.py
+++ docker_test_app/views.py
@@ -4,6 +4,6 @@ from docker_test_app.models import Students
 
 # Create your views here.
 def home(request):
-    students = Students.objects.all().first()
-    print("sutudent name :", students.name)
-    return HttpResponse(f"<h1>....Welccome to home page Client....Student name :: {students.name}........</h1>")
+    # students = Students.objects.all().first()
+    # print("sutudent name :", students.name)
+    return HttpResponse(f"<h1>....Welccome to home page Client............</h1>")
